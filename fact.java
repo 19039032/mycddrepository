c11a2fdf2782c875a6cf1ffe7a335c5792ff1c78
19039032 <mchaitanygupta@gmail.com>
2023-12-11 10:33:04 +0530

    fact

---
+++ fact.java
@@ -0,0 +1,12 @@
+class fact{
+	static int factorial(int n){
+		if(n==1)return 1;
+		else{
+		return n*factorial(n-1);
+		}
+	}
+	public static void main(String[] args){
+		int f=factorial(5);
+		System.out.println("5 factorial is "+f);
+	}
+}
